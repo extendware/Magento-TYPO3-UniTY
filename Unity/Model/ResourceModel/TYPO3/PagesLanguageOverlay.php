28618fc6c5b55fb790527a7f982400b1104b0e12
Fenil Timbadiya <fenil@web-vision.de>
2023-07-13 18:07:35 +0200

    [Task] EW-528 M2 Extension mageUnity Implementation Changes

--- Unity/Model/ResourceModel/TYPO3/PagesLanguageOverlay.php
+++ Unity/Model/ResourceModel/TYPO3/PagesLanguageOverlay.php
@@ -5,6 +5,6 @@ class PagesLanguageOverlay extends AbstractModuleResource
 {
     protected function _construct()
     {
-        $this->_init('pages_language_overlay', 'uid');
+        $this->_init('pages', 'uid');
     }
 }
